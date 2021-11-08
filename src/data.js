c1950a5e7e66c171d9062e0af55204e60cbad9f6
Shimanti <shimanti@gmail.com>
2021-11-08 13:54:39 -0700

    final project

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    id: 1,
+    title: 'Why are you learning React.js?',
+    info:
+      'To implement APIs and to create new websites',
+  },
+  {
+    id: 2,
+    title: 'How do I learn React.js?',
+    info:
+      'Learn HTML, CSS, JavaScript, React. Then start building simple projects.',
+  },
+  {
+    id: 3,
+    title: 'Why do you need to learn so much?',
+    info:
+      "You don't. If you learn HTML and CSS, you can revamp websites from being unaccessible to accessible. Or you can learn design, UXUI, and tell others how to layout their sites. Adding JavaScript means that you can also code.",
+  },
+  {
+    id: 4,
+    title: 'Why are you learning to code?',
+    info:
+      'Software development allows me to create sites. With the knowledge that I have, I can build the tools for tomorrow.',
+  }
+]
+export default questions
